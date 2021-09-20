ec75b3556d88099edacca45d6670ce5f836224c9
UmmarHamid <kaxama34@gmail.com>
2021-09-20 13:16:03 +0530

    added support for all stories from storyblok

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
+import React from "react"
 import Head from "next/head"
 import styles from "../styles/Home.module.scss"
 import Page from '../components/Page'
-// The Storyblok Client
-import Storyblok from "../lib/storyblok"
+// The Storyblok Client and hook
+import Storyblok, { useStoryblok } from "../lib/storyblok"
 import Layout from "../components/Layout"
-export default function Home(props) {
-  const story = props.story
-
+export default function Home({ story, preview, Sdata }) {
+  //set single story
+  story = useStoryblok(story, preview)
+  //set all stories
+  const stories = Sdata.data.stories
+  // add stoies to state
+  const [storyBlokData, setStoryBlokData] = React.useState(stories);
+  //use stories
+  {
+    storyBlokData.map(
+      (item) => console.log(item)
+    )
+  }
   return (
     <div >
       <Head>
         <title>Storyblok Project</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="//app.storyblok.com/f/storyblok-v2-latest.js" type="text/javascript" id="storyblokBridge">
+        </script>
       </Head>
 
       <header>
         <Layout>
           <Page content={story.content} />
+
         </Layout>
 
       </header>
@@ -27,7 +41,7 @@ export default function Home(props) {
 }
 
 export async function getStaticProps(context) {
-  let slug = "home"
+  let slug = "hero"
   let params = {
     version: "draft", // or 'published'
   }
@@ -37,13 +51,18 @@ export async function getStaticProps(context) {
     params.cv = Date.now()
   }
 
-  let { data } = await Storyblok.get(`cdn/stories/${slug}`, params)
+  let { data } = await Storyblok.get(`cdn/stories/project/components/${slug}`, params)
+  //Get all data here
+  let Sdata = await Storyblok.get('cdn/stories/', {
+    version: "draft"
 
+  })
   return {
     props: {
       story: data ? data.story : false,
-      preview: context.preview || false
+      preview: context.preview || false,
+      Sdata
     },
-    revalidate: 10,
+    revalidate: 3600, // revalidate every hour
   }
 }

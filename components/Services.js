246aed7bc15dd2e239791f6759d463dee736d54d
UmmarHamid <kaxama34@gmail.com>
2021-09-21 10:10:49 +0530

    added dynamic service header from story blok

--- components/Services.js
+++ components/Services.js
@@ -2,6 +2,11 @@ import styles from "../styles/Services.module.scss"
 import React from 'react'
 
 const Services = ({ stories }) => {
+    // console.log(stories)
+    const header = stories.filter((story) =>
+        story.slug == "services-header"
+    )
+    // console.log(header)
     const services = stories.filter((story) =>
         story.parent_id == 72420272
     )
@@ -11,17 +16,17 @@ const Services = ({ stories }) => {
             <div className={styles.group}>
                 <div className={styles.group__heading}>
                     <div className={styles.group__heading__title}>
-                        Kalite ve Süreç Yönetimi
+                        {header[0].content.Title}
                     </div>
                     <div className={styles.group__heading__description}>
-                        Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.
+                        {header[0].content.Text}
                     </div>
                 </div>
                 <div className={styles.group__services}>
                     <div className={styles.group__services__row}>
 
                         {services.map((service) => {
-                            console.log(service)
+                            // console.log(service)
 
                             return (
                                 <div className={styles.group__services__row__service} key={service.id}>
